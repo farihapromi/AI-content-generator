93919c218ded9abfab2f616fd7f3e4b949595a8f
Fariha Promi <farihapromi2611@gmail.com>
2025-05-02 22:22:27 +0600

    Add clerk for authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Our App</h1>
+      <h1 className='p-2 m-2'>Welcome to Our App</h1>
+      <Button>Subscribe</Button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#704ef8',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
