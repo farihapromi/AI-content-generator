864ed53e89d0ba056af8dc9643cdf2f051fa7ff5
Fariha Promi <farihapromi2611@gmail.com>
2025-05-03 21:00:22 +0600

    Add SearchSection button property

--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -1,9 +1,23 @@
+import { Search } from 'lucide-react';
 import React from 'react';
 
 const SearchSection = () => {
   return (
-    <div className='p-10 bg-gradient-to-br from-purple-500 via-purple-700 to-blue-600'>
-      <h2>Browse all templates</h2>
+    <div className='p-10 bg-gradient-to-br from-purple-500 via-purple-700 to-blue-600 flex flex-col items-center justify-center text-white'>
+      <h2 className='text-3xl font-bold'>Browse All templates</h2>
+      <p>What would you like to create today?</p>
+      <div className='w-full flex justify-center'>
+        <div className='flex gap-2 items-center p-2 border rounded-lg bg-white my-5 w-[50%]'>
+          <Search className='text-primary' />
+          <input
+            type='text'
+            name=''
+            id=''
+            placeholder='Search'
+            className='bg-transparent border-none focus:outline-none focus:ring-0 focus:border-transparent w-ful text-black'
+          />
+        </div>
+      </div>
     </div>
   );
 };
