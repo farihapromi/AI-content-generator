79243e1602bd36e3c68c8aa3a056b042a234d932
Fariha Promi <farihapromi2611@gmail.com>
2025-05-03 19:39:17 +0600

    Add sidebar menuList with lucid icon

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,10 +1,35 @@
+import { FileClock, Home, Settings, WalletCards } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
 
 const SideNav = () => {
+  const menuList = [
+    {
+      name: 'Home',
+      icon: Home,
+      path: '/dashboard',
+    },
+    {
+      name: 'History',
+      icon: FileClock,
+      path: '/dashboard/history',
+    },
+    {
+      name: 'Billing',
+      icon: WalletCards,
+      path: '/dashboard/billing',
+    },
+    {
+      name: 'Seting',
+      icon: Settings,
+      path: '/dashboard/setting',
+    },
+  ];
   return (
-    <div>
-      <Image src={'/logo.svg'} alt='logo' height={100} width={100} />
+    <div className='h-screen p-5 shadow-sm border'>
+      <div className='flex justify-center'>
+        <Image src={'/logo.svg'} alt='logo' height={50} width={50} />
+      </div>
     </div>
   );
 };
