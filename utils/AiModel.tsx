27a02ceda4276ca89db9f28e6cfe709b934dabe0
Fariha Promi <farihapromi2611@gmail.com>
2025-05-04 21:26:02 +0600

    Integrate Gemini

---
+++ utils/AiModel.tsx
@@ -0,0 +1,25 @@
+// utils/AIModel.tsx
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from '@google/generative-ai';
+
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY!);
+
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: 'text/plain',
+};
+
+export const chatSession = model.startChat({
+  generationConfig,
+
+  history: [],
+});
