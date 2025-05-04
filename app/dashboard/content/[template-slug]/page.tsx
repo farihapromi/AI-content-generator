a4d46074510e6a1620a041583d5c2eb914ac8eea
Fariha Promi <farihapromi2611@gmail.com>
2025-05-04 11:36:00 +0600

    Add FormSection

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,4 +1,7 @@
+import Templates from '@/app/(data)/Templates';
 import React from 'react';
+import FormSection from '../../_components/FormSection';
+import OutputSection from '../../_components/OutputSection';
 interface PROPS {
   params: {
     'template-slug': string;
@@ -6,7 +9,18 @@ interface PROPS {
 }
 
 const CreateNewContent = (props: PROPS) => {
-  return <div>create</div>;
+  //filter seelcted tamplte
+  const selectedTemplate: TEMPLATE = Templates?.find(
+    (item) => item.slug === props.params['template-slug']
+  );
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 gap -4 p-5'>
+      {/* FormSection */}
+      <FormSection selectedTemplate={selectedTemplate} />
+      {/* OutputSection */}
+      <OutputSection />
+    </div>
+  );
 };
 
 export default CreateNewContent;
