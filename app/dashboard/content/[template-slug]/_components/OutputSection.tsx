175f304c86188973c2af40c8f245b3fdd9a05ae7
Fariha Promi <farihapromi2611@gmail.com>
2025-05-04 20:19:29 +0600

    Add react toast editor for adding rich text editor

--- app/dashboard/content/[template-slug]/_components/OutputSection.tsx
+++ app/dashboard/content/[template-slug]/_components/OutputSection.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/react-editor';
 
 const OutputSection = () => {
   return <div>Outputsection</div>;
