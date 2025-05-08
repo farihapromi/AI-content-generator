16580dc652eee17cafd7b1effffdb1adeb7c74ee
Fariha Promi <farihapromi2611@gmail.com>
2025-05-08 21:55:06 +0600

    Update sdk of strip

--- app/api/payment/initiate/route.ts
+++ app/api/payment/initiate/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-10-16',
+    apiVersion: '2025-04-30.basil',
+
 });
 
 export async function POST(req: NextRequest) {
