341511a628e5420f799066502572f3b87944d533
Fariha Promi <farihapromi2611@gmail.com>
2025-05-08 21:04:19 +0600

    Update search

--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -7,7 +7,7 @@ const SearchSection = ({ onSearchInput }: any) => {
       <h2 className='text-3xl font-bold'>Browse All templates</h2>
       <p>What would you like to create today?</p>
       <div className='w-full flex justify-center'>
-        <div className='flex gap-2 items-center p-2 border rounded-lg bg-white my-5 w-[50%]'>
+        <div className='flex gap-3 items-center p-2 border rounded-lg bg-white my-5 w-[50%]'>
           <Search className='text-primary' />
           <input
             type='text'
